ba89eeaf9e3febdda61574c6d768e8d64af97bd0
Reema dhanwani <reemadhanwani55@gmail.com>
2025-03-16 19:10:50 +0530

    Added city router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
-const CityRepository = require("./repository/city-repository");
+const ApiRoutes = require('./routes/index');
 
 
 const setupAndStartServer = async () => {
@@ -11,10 +11,10 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.use('/api',ApiRoutes);
+
     app.listen(PORT,() => {
         console.log(`Server started at ${PORT}`);
-        const repo = new CityRepository();
-        repo.createCity({name: "New Delhi"});
     });
 }
 

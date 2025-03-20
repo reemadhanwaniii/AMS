93a5bbcc618ce06bf0c4543fdd6767ce031518ec
Reema dhanwani <reemadhanwani55@gmail.com>
2025-03-20 13:18:04 +0530

    Added flight create request middlewares

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const { FlightMiddlewares } = require('../../middlewares/index');
+
 const CityController = require('../../controllers/city-controller');
 const FlightController = require('../../controllers/flight-controller');
 const AirportController = require('../../controllers/airport-controller');
@@ -12,7 +15,11 @@ router.get('/city/:id',CityController.get);
 router.patch ('/city/:id',CityController.update);
 router.get('/city',CityController.getAll);
 
-router.post('/flights',FlightController.create);
+router.post(
+    '/flights',
+    FlightMiddlewares.validateCreateFlight,
+    FlightController.create
+);
 router.get('/flights',FlightController.getAll);
 
 
